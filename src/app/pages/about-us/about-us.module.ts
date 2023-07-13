4d48f2725bb4fa0e990464520aee696dc652ff44
rakushka-san <vladislavmohov@mail.ru>
2023-07-13 18:46:21 +0300

    Миссия и видение

--- src/app/pages/about-us/about-us.module.ts
+++ src/app/pages/about-us/about-us.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AboutUsComponent } from './about-us.component';
 import { AboutUsHeadingComponent } from './about-us-heading/about-us-heading.component';
+import { MissionVisionComponent } from './mission-vision/mission-vision.component';
 
 @NgModule({
-  declarations: [AboutUsComponent, AboutUsHeadingComponent],
+  declarations: [AboutUsComponent, AboutUsHeadingComponent, MissionVisionComponent],
   imports: [CommonModule],
 })
 export class AboutUsModule {}
