e8442960471a412a22ffc7e4b9046b39bb885787
rakushka-san <vladislavmohov@mail.ru>
2023-07-13 19:00:12 +0300

    Сведения о команде

--- src/app/pages/about-us/about-us.module.ts
+++ src/app/pages/about-us/about-us.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AboutUsComponent } from './about-us.component';
 import { AboutUsHeadingComponent } from './about-us-heading/about-us-heading.component';
 import { MissionVisionComponent } from './mission-vision/mission-vision.component';
+import { AboutTeamComponent } from './about-team/about-team.component';
 
 @NgModule({
-  declarations: [AboutUsComponent, AboutUsHeadingComponent, MissionVisionComponent],
+  declarations: [AboutUsComponent, AboutUsHeadingComponent, MissionVisionComponent, AboutTeamComponent],
   imports: [CommonModule],
 })
 export class AboutUsModule {}
