b340679ff10a08443c768f1054f5d505fba7c7ec
rakushka-san <vladislavmohov@mail.ru>
2023-08-02 01:28:26 +0300

    Компонент блока с постами главной страницы

--- src/app/components/pages/home/featured-posts/featured-posts.component.ts
+++ src/app/components/pages/home/featured-posts/featured-posts.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPost } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-featured-posts',
   templateUrl: './featured-posts.component.html',
-  styleUrls: ['./featured-posts.component.scss']
+  styleUrls: ['./featured-posts.component.scss'],
 })
-export class FeaturedPostsComponent {
-    exampleAuthor = "John Doe";
-    exampleDate = new Date("2022-05-23");
-    exampleTitle = "8 Figma design systems that you can download for free today.";
+export class FeaturedPostsComponent implements OnInit {
+  exampleAuthor = 'John Doe';
+  exampleDate = new Date('2022-05-23');
+  exampleTitle = '8 Figma design systems that you can download for free today.';
+
+  featuredPost!: IPost;
+  posts: IPost[] = [];
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.postsService.getPosts().subscribe((posts) => {
+      this.featuredPost = posts[1];
+      this.posts = posts.slice(2, 6);
+    });
+  }
 }
