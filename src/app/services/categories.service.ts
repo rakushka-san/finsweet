54ef39ac47c603770b3581b16e88065349121e8b
rakushka-san <vladislavmohov@mail.ru>
2023-08-01 15:43:31 +0300

    Сервис для работы с категориями

---
+++ src/app/services/categories.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Observable } from 'rxjs';
+import { ICategory } from '../models/category';
+
+const URL = `${environment.apiUrl}categories`;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<ICategory[]> {
+    return this.http.get<ICategory[]>(URL);
+  }
+}
