326cbc2ec16e9052aad33e13fe766e4fd4595608
rakushka-san <vladislavmohov@mail.ru>
2023-08-02 15:52:35 +0300

    Обработка неопределенных данных

--- src/app/components/pages/home/home-hero/home-hero.component.ts
+++ src/app/components/pages/home/home-hero/home-hero.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IPost } from 'src/app/models/post';
 import { PostsService } from 'src/app/services/posts.service';
-import { mergeMap } from 'rxjs/operators';
-import { forkJoin } from 'rxjs';
-import { AuthorsService } from 'src/app/services/authors.service';
-import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-home-hero',
@@ -12,21 +8,7 @@ import { CategoriesService } from 'src/app/services/categories.service';
   styleUrls: ['./home-hero.component.scss'],
 })
 export class HomeHeroComponent implements OnInit {
-  featuredPost: IPost = {
-    _id: '',
-    title: '',
-    description: '',
-    category: '',
-    categoryName: '',
-    author: '',
-    authorName: '',
-    date: new Date(),
-    imgSrc: '',
-    content: '',
-  };
-
-  categoryName: string = '';
-  authorName: string = '';
+  featuredPost: IPost | undefined;
 
   constructor(private postsService: PostsService) {}
 
