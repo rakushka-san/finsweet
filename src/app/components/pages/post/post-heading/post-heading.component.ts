eb36c4e4bdedb634b4117f433733bc665441d344
rakushka-san <vladislavmohov@mail.ru>
2023-08-02 21:04:10 +0300

    Компонент заглавия страницы поста

--- src/app/components/pages/post/post-heading/post-heading.component.ts
+++ src/app/components/pages/post/post-heading/post-heading.component.ts
@@ -1,18 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, forkJoin, mergeMap, switchMap } from 'rxjs';
+import { IAuthor } from 'src/app/models/author';
+import { ICategory } from 'src/app/models/category';
+import { IPost } from 'src/app/models/post';
+import { AuthorsService } from 'src/app/services/authors.service';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-post-heading',
   templateUrl: './post-heading.component.html',
   styleUrls: ['./post-heading.component.scss'],
 })
-export class PostHeadingComponent {
-  @Input() profilePictureSrc: string =
-    './../../../../../assets/img/Profile picture.jpg';
-  @Input() authorName: string = 'Andrew Jonson';
-  @Input() authorId: string = '';
-  @Input() date: Date = new Date();
-  @Input() title: string = 'Step-by-step guide to choosing great font pairs';
-  @Input() categoryIconSrc: string =
-    './../../../../../assets/img/startup-icon.svg';
-  @Input() category: string = 'Startup';
+export class PostHeadingComponent implements OnInit {
+  post$: Observable<IPost> | undefined;
+  author$: Observable<IAuthor> | undefined;
+  category$: Observable<ICategory> | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private authorsService: AuthorsService,
+    private categoriesService: CategoriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.post$ = this.route.paramMap.pipe(
+      switchMap((paramMap) => paramMap.getAll('id')),
+      mergeMap((id) => {
+        return this.postsService.getPost(id);
+      })
+    );
+
+    this.author$ = this.post$.pipe(
+      mergeMap((post) => {
+        return this.authorsService.getAuthor(post.author);
+      })
+    );
+
+    this.category$ = this.post$.pipe(
+      mergeMap((post) => {
+        return this.categoriesService.getCategory(post.category);
+      })
+    );
+  }
 }
