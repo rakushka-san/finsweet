f8755b00ac4fe2e959026ac5d3fb236d65c9c492
rakushka-san <vladislavmohov@mail.ru>
2023-08-02 21:35:49 +0300

    Компонент контента поста

--- src/app/components/pages/post/post-content/post-content.component.ts
+++ src/app/components/pages/post/post-content/post-content.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, mergeMap, switchMap } from 'rxjs';
+import { IPost } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-post-content',
   templateUrl: './post-content.component.html',
   styleUrls: ['./post-content.component.scss'],
 })
-export class PostContentComponent {
+export class PostContentComponent implements OnInit {
   @Input() imgSrc: string = './../../../../../assets/img/post-img.jpg';
+
+  post$: Observable<IPost> | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
+
+  ngOnInit(): void {
+    this.post$ = this.route.paramMap.pipe(
+      switchMap((paramMap) => paramMap.getAll('id')),
+      mergeMap((id) => {
+        return this.postsService.getPost(id);
+      })
+    );
+  }
 }
